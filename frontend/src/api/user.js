85c215ffd81afffa0ea9fb29b4eefedc372dbc68
Arthur Glandier <arthur.glandier@gmail.com>
2026-04-22 23:21:25 +0200

    frontend: updateTeamlist prend battleroom_id, ajout getBattleroom

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -9,12 +9,18 @@ export class UserApi {
     return this.#client.get('/user/secured', { query: { name } })
   }
 
-  getStats(name) {
-    return this.#client.get('/user/stats', { query: { name } })
+  getStats(name, battleroom_id = null) {
+    const query = { name }
+    if (battleroom_id !== null) query.battleroom_id = battleroom_id
+    return this.#client.get('/user/stats', { query })
   }
 
-  updateTeamlist(teamlist) {
-    return this.#client.post('/user/teamlist', { body: { teamlist } })
+  getBattleroom() {
+    return this.#client.get('/user/battleroom')
+  }
+
+  updateTeamlist(battleroom_id, teamlist) {
+    return this.#client.post('/user/teamlist', { body: { battleroom_id, teamlist } })
   }
 
   getActiveBattle(name) {
